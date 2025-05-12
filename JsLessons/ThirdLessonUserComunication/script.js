59c4be5bca141aff5ad1cabc68939c2e967a920b
aliishov <alishov7394@gmail.com>
2025-05-12 18:41:23 +0400

    JS: User Comunication Alert, Confirm, Prompt

---
+++ JsLessons/ThirdLessonUserComunication/script.js
@@ -0,0 +1,19 @@
+// alert('Hello');
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer1 = prompt("You older that 18", "Yes");
+// console.log(answer1, typeof(answer1));
+// console.log(answer1 + 5);
+
+// const answer2 = +prompt("Enter your age");
+// console.log(answer2, typeof(answer2));
+
+const answers = [];
+answers[0] = prompt("Enter your name:");
+answers[1] = prompt("Enter your surname:");
+answers[2] = prompt("Enter your age:");
+document.write(answers); // write - depricated, use only for education
+console.log(typeof(answers));
+console.log(typeof(null)); // Object
