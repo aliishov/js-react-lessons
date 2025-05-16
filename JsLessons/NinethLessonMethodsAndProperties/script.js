ad303586ed4026bbe2529fa8cdce402ae44f5c0a
aliishov <alishov7394@gmail.com>
2025-05-16 16:37:30 +0400

    JS: Methods and Properties

---
+++ JsLessons/NinethLessonMethodsAndProperties/script.js
@@ -0,0 +1,23 @@
+const str = "test";
+const arr = [1, 2, 3];
+
+console.log(str.length, arr.length); // Property
+
+console.log(str.toUpperCase()); // Method
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+console.log(fruit.slice(0, 4));
+console.log(fruit.slice(5));
+console.log(fruit.substring(0, 4));
+console.log(fruit.substring(5));
+console.log(fruit);
+
+// Math Library
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseFloat(test));
+console.log(parseInt(test));
